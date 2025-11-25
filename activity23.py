38d2d860518350e86aeab0d8cc2e80d48e5bf48e
iyaaabananaaa <sofiacapuchino14@gmail.com>
2025-11-25 20:40:05 +0800

    ACTIVITY 23

---
+++ activity23.py
@@ -0,0 +1,14 @@
+posa1 = 'millie'
+posa2 = 'yuri'
+
+posa = ['millie', 'yuri', 'mecko', 'mocha', 'kiwi', 'minnie', 'orange', 'tootie']
+
+print(posa)
+print(posa[3])
+print(posa[3 : 6])
+
+posa.append("whitey")
+print(posa)
+
+posa.pop()
+print(posa)
