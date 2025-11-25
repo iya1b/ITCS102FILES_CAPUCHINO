6a115277e15d835f7f6749c243ce62fd635d49fa
iyaaabananaaa <sofiacapuchino14@gmail.com>
2025-11-25 20:56:32 +0800

    ACTIVITY 24

---
+++ activity24.py
@@ -0,0 +1,10 @@
+from activity24_1 import Greetpname, GreetInfo
+
+def Greet():
+    print("Hi there, adventurer. Welcome!")
+    print("Feel free to browse around.")
+
+Greet()
+Greetpname('iya')
+Greetpname('roi')
+GreetInfo('iya', 'lucena', '18')
